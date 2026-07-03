b4bbb797f40371e6ab6739ea9f83df6fbb70fb0d
Claudiane2025 <claudianefauro@gmail.com>
2026-07-03 16:36:32 +0000

    criacao tarefa

--- main.py
+++ main.py
@@ -20,6 +20,16 @@ def index():
 
 @APP.get("/tarefas")
 def listar_tarefas():
+    global LISTA_TAREFAS
+
     # Lista tarefas (somente id e titulo)
     if len(LISTA_TAREFAS) == 0:
         return LISTA_TAREFAS
+
+    tarefas = []
+    
+    for tarefa in LISTA_TAREFAS:
+        info = {"id": tarefa['id'], "titulo": tarefa['titulo']}
+        tarefas.append(info)
+
+    return tarefas
