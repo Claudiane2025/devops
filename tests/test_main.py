5823c9658e5a510ccfdf73ca67a2d867e6c0fab5
Claudiane2025 <claudianefauro@gmail.com>
2026-07-04 14:14:36 +0000

    Adicionado testes para main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,21 @@
-def soma(a, b):
-    return a + b
+from fastapi.testclient import TestClient
 
-def test_soma_positivos():
-    assert soma(4, 4) == 8
+from app import APP
 
-def test_soma_errada():
-    assert soma(3, 7) == 10
+CLIENT = TestClient(APP)
+
+def test_index():
+    requisicao = CLIENT.get("/")
+
+    assert requisicao.status_code == 200
+    assert requisicao.json() == "Olá, DevOps!"
+
+def test_criar_tarefa():
+    requisicao = CLIENT.post("/tarefas?id=0&titulo=tarefa&descricao=descricao-tarefa")
+
+    assert requisicao.status_code == 201
+    assert requisicao.json() == {"mensagem": "OK"}
+
+    requisicao = CLIENT.post("/tarefas?id=0&titulo=tarefa&descricao=descricao-tarefa")
+    assert requisicao.status_code == 202
+    assert requisicao.json()['detail'] == {"mensagem": "TAREFA JÁ EXISTE!"}
