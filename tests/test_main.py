7e97d86ae1ef35ac55ad56016f3a871a8270a3d1
Claudiane <claudianefauro@gmail.com>
2026-07-18 15:48:17 -0300

    Testes arrumados

--- tests/test_main.py
+++ tests/test_main.py
@@ -13,6 +13,12 @@ def test_index():
     assert requisicao.status_code == 200
     assert requisicao.json() == "Olá, DevOps!"
 
+# Criar um teste unitário para validar se a tarefa foi criada com sucesso
+# CLIENT.post(...) (substituir pela string para criação de tarefa)
+# Verificar se o código de status é 201
+# Verificar se o retorno, quando tarefa é criada, é igual a {"mensagem": "OK"} ou conforme definido na sua API
+# Verificar se o retorno, quando a tarefa já existe, é igual a {"mensagem" : "TAREFA JÁ EXISTE"} ou conforme definido na sua API
+
 def test_criar_tarefa():
     requisicao = CLIENT.post("/tarefas?id=0&titulo=tarefa&descricao=descricao-tarefa")
 
@@ -45,4 +51,27 @@ def test_atualizar_tarefa():
     assert requisicao.status_code == 200
     assert requisicao.json()["titulo"] == "tarefa_mock"
 
+def test_verificar_tarefa_especifica():
+    criar_tarefa_mock()
+    requisicao = CLIENT.get("/tarefas/0")
 
+    assert requisicao.status_code == 200
+
+    dados = requisicao.json()
+    assert dados["titulo"] == "tarefa_mock"
+    assert dados["descricao"] == "descricao-tarefa"
+    assert dados["id"] == 0
+    assert dados["concluido"] == False
+
+    requisicao = CLIENT.get("/tarefas/5")
+    CLIENT.delete('/tarefas/0')
+
+    assert requisicao.json() == {"mensagem": "Não existe nenhuma tarefa"}
+
+def test_health():
+    req = CLIENT.get("/health")
+
+    assert req.status_code == 200
+    assert req.json() == {"mensagem": "healthy"}
+    
+ 
